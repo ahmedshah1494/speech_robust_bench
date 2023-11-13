7c3bc65c6ec18d7136e8e94dfda8e61ab562b9e2
ahmed <ahmedshah1494@gmail.com>
2023-11-13 11:58:32 -0500

    moved model info

---
+++ model_info.py
@@ -0,0 +1,45 @@
+model_to_params = {
+    'whisper-large-v2': 1550e6,
+    'wav2vec2-large-960h-lv60-self': 317e6,
+    'wav2vec2-large-960h': 317e6,
+    'wav2vec2-base-960h': 95e6,
+    'hubert-large-ls960-ft': 300e6,
+    'whisper-tiny.en': 39e6,
+    'whisper-tiny': 39e6,
+    'wav2vec2-large-robust-ft-libri-960h': 317e6,
+    'deepspeech': 86e6,
+    'emformer-rnnt': 76e6,
+    'mms-1b-fl102': 964648576,
+    'wav2vec2-base-10k-voxpopuli-ft-es': 94371712,
+    'wav2vec2-large-xlsr-53-spanish': 315438720
+}
+model_to_traindata = {
+    'whisper-large-v2': 680000,
+    'wav2vec2-large-960h-lv60-self': 60000,
+    'wav2vec2-large-960h': 960,
+    'wav2vec2-base-960h': 960,
+    'hubert-large-ls960-ft': 60000,
+    'whisper-tiny.en': 563000,
+    'whisper-tiny': 680000,
+    'wav2vec2-large-robust-ft-libri-960h': 63000,
+    'deepspeech': 960,
+    'emformer-rnnt': 960,
+    'mms-1b-fl102': 55000,
+    'wav2vec2-base-10k-voxpopuli-ft-es': 10116,
+    'wav2vec2-large-xlsr-53-spanish': 1350
+}
+model_to_year = {
+    'whisper-large-v2': 2023,
+    'wav2vec2-large-960h-lv60-self': 2021,
+    'wav2vec2-large-960h': 2020,
+    'wav2vec2-base-960h': 2020,
+    'hubert-large-ls960-ft': 2021,
+    'whisper-tiny.en': 2023,
+    'whisper-tiny': 2023,
+    'wav2vec2-large-robust-ft-libri-960h': 2021,
+    'deepspeech': 2016,
+    'emformer-rnnt': 2020,
+    'mms-1b-fl102': 2023,
+    'wav2vec2-base-10k-voxpopuli-ft-es': 2021,
+    'wav2vec2-large-xlsr-53-spanish': 2020
+}
