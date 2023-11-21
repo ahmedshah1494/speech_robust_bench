d3545de36392e30b062571cfe5a60e1d09c73c37
ahmed <ahmedshah1494@gmail.com>
2023-11-20 20:25:34 -0800

    moved corruption classes to different file and added option for perturbation robustness test

---
+++ corruptions.py
@@ -0,0 +1,185 @@
+import torch
+import torchaudio.transforms as audio_transforms
+import torchaudio
+from torchaudio import functional as F
+import numpy as np
+import pandas as pd
+import os
+
+class UniformNoise(torch.nn.Module):
+    def __init__(self, snr) -> None:
+        super().__init__()
+        self.snr = snr
+    
+    def __repr__(self):
+        return f"UniformNoise({self.snr} dB)"
+    
+    def forward(self, x, *args, **kwargs):
+        if not isinstance(x, torch.Tensor):
+            x = torch.FloatTensor(x)
+        # if not isinstance(xlen, torch.Tensor):
+        #     xlen = torch.LongTensor()
+        d = torch.empty_like(x).uniform_(-1, 1)
+        snr = torch.zeros(x.shape[:-1], device=x.device) + self.snr
+        return F.add_noise(x, d, snr)
+
+class GaussianNoise(torch.nn.Module):
+    def __init__(self, snr) -> None:
+        super().__init__()
+        self.snr = snr
+    
+    def __repr__(self):
+        return f"GaussianNoise({self.snr} dB)"
+    
+    def forward(self, x, *args, **kwargs):
+        if not isinstance(x, torch.Tensor):
+            x = torch.FloatTensor(x)
+        # if not isinstance(xlen, torch.Tensor):
+        #     xlen = torch.LongTensor()
+        rng = torch.Generator(x.device)
+        rng = rng.manual_seed(rng.seed())
+        d = torch.empty_like(x).normal_(0, 1, generator=rng)
+        snr = torch.zeros(x.shape[:-1], device=x.device) + self.snr
+        return F.add_noise(x, d, snr)
+
+class EnvNoise(torch.nn.Module):
+    seeds = [4117371, 7124264, 1832224, 8042969, 4454604, 5347561, 7059465,
+                3774329, 1412644, 1519183, 6969162, 7885564, 3707167, 5816443,
+                9477077, 9822365, 7482569, 7792808, 9120101, 5467473]
+    
+    def __init__(self, snr, noise_dir='/ocean/projects/cis220031p/mshah1/audio_robustness_benchmark/MS-SNSD/noise_test') -> None:
+        super().__init__()
+        self.snr = snr
+        self.noise_dir = noise_dir
+        self.noise_files = [x for x in os.listdir(noise_dir) if x.endswith('.wav')]
+        seed = self.seeds[int(snr % len(self.seeds))]
+        self.rng = np.random.default_rng(seed)
+    
+    def __repr__(self):
+        return f"EnvNoise({self.snr} dB)"
+
+    def forward(self, x, *args, **kwargs):
+        if not isinstance(x, torch.Tensor):
+            x = torch.FloatTensor(x)
+        xlen = x.shape[-1]
+        noise_file = os.path.join(self.noise_dir, self.noise_files[self.rng.choice(len(self.noise_files))])
+        noise_raw, sample_rate = torchaudio.load(noise_file)
+        noise = noise_raw[..., :xlen]
+        while noise.shape[-1] < xlen:
+            noise = torch.cat([noise, noise], -1)
+            noise = noise[..., :xlen]
+        noise = noise[0].reshape(-1).to(x.device)
+        snr = torch.zeros(x.shape[:-1], device=x.device) + self.snr
+        x_ = F.add_noise(x, noise, snr)
+        return x_
+
+
+class RIR(torch.nn.Module):
+    seed = 9983137
+    def __init__(self, sev, rir_dir='/ocean/projects/cis220031p/mshah1/audio_robustness_benchmark/RIRS_NOISES/simulated_rirs', rir_snr_file='rir_snr.csv') -> None:
+        super().__init__()
+        assert sev <= 4
+        self.rir_dir = rir_dir
+        # self.rir_files = [x for x in os.listdir(rir_dir) if x.endswith('.wav')]
+        rir_files = []
+        for root, dirs, files in os.walk(rir_dir):
+            for name in files:
+                if name.endswith('wav'):
+                    rir_files.append(os.path.join(root, name))
+        rir_snr_df = pd.read_csv(rir_snr_file)
+        unique_snrs = rir_snr_df['snr'].unique()
+        snr_sevs = np.linspace(unique_snrs.max(), unique_snrs.min(), 5)
+        print(snr_sevs, snr_sevs[sev])
+        if sev == 0:
+            filtered_rows = rir_snr_df[rir_snr_df['snr'] >= snr_sevs[sev]]
+        elif sev == 1:
+            filtered_rows = rir_snr_df[(snr_sevs[sev] <= rir_snr_df['snr']) & (rir_snr_df['snr'] <= snr_sevs[sev-1])]
+        else:
+            filtered_rows = rir_snr_df[(snr_sevs[sev] <= rir_snr_df['snr']) & (rir_snr_df['snr'] < snr_sevs[sev-1])]
+        self.rir_files = filtered_rows['filename'].values
+        print(filtered_rows["snr"].min(), filtered_rows["snr"].max())
+        print(f'using {len(self.rir_files)} rirs with average SNR={filtered_rows["snr"].mean()}')
+        # {x['filename']: x['snr'] for x in pd.read_csv(rir_snr_file).to_dict('records')}
+        self.rng = np.random.default_rng(self.seed)
+
+    def forward(self, x, *args, **kwargs):
+        if not isinstance(x, torch.Tensor):
+            x = torch.FloatTensor(x)
+        xlen = x.shape[-1]
+        def get_random_rir():
+            rir_file = os.path.join(self.rir_dir, self.rir_files[self.rng.choice(len(self.rir_files))])
+            rir_raw, sample_rate = torchaudio.load(rir_file)
+            rir = rir_raw[:, int(sample_rate * .01) : ]
+            return rir
+        rir = get_random_rir()
+        rir = rir / torch.norm(rir, p=2)
+        rir = rir[0].reshape(-1).to(x.device)
+        x_ = torchaudio.functional.fftconvolve(x, rir)
+        return x_
+
+class Speed(torch.nn.Module):
+    def __init__(self, factor, orig_freq=16000) -> None:
+        super().__init__()
+        self.factor = factor
+        self.transform = audio_transforms.Speed(orig_freq, factor)
+    
+    def forward(self, x, *args, **kwargs):
+        if not isinstance(x, torch.Tensor):
+            x = torch.FloatTensor(x)
+        xlen = x.shape[-1]
+        x_, new_lens =  self.transform.to(x.device)(x)
+        return x_
+    
+class Pitch(torch.nn.Module):
+    def __init__(self, shift, orig_freq=16000) -> None:
+        super().__init__()
+        self.shift = shift
+        self.transform = audio_transforms.PitchShift(orig_freq, shift)
+    
+    def forward(self, x, *args, **kwargs):
+        if not isinstance(x, torch.Tensor):
+            x = torch.FloatTensor(x)
+        xlen = x.shape[-1]
+        x_ =  self.transform.to(x.device)(x)
+        return x_
+
+class VoiceConversion(torch.nn.Module):
+    seed = 9983137
+    def __init__(self, accents) -> None:
+        super().__init__()
+        self.accents = accents
+        from transformers import SpeechT5Processor, SpeechT5ForTextToSpeech, SpeechT5HifiGan
+        self.processor = SpeechT5Processor.from_pretrained("microsoft/speecht5_tts")
+        self.model = SpeechT5ForTextToSpeech.from_pretrained("microsoft/speecht5_tts")
+        self.vocoder = SpeechT5HifiGan.from_pretrained("microsoft/speecht5_hifigan")
+        ds = load_dataset("Matthijs/cmu-arctic-xvectors", split="validation")
+        self.ds = ds.filter(lambda x: x['filename'].split('_')[2] in accents)
+        self.rng = np.random.default_rng(self.seed)
+    
+    def __repr__(self):
+        return f"VoiceConversion({self.accents})"
+    
+    def forward(self, speech, text, *args, **kwargs):
+        if len(self.ds) == 0:
+            return speech
+        voice_idxs = self.rng.choice(len(self.ds))
+        speaker_embeddings = torch.tensor(self.ds[voice_idxs]["xvector"]).cuda().unsqueeze(0)
+        inputs = self.processor(text=text, return_tensors="pt")
+        speech = self.model.generate_speech(inputs["input_ids"].cuda(), speaker_embeddings, vocoder=self.vocoder)
+        return speech
+    
+class Compose(torch.nn.Module):
+    def __init__(self, transforms) -> None:
+        super().__init__()
+        self.transforms = torch.nn.ModuleList(transforms)
+    
+    def __repr__(self):
+        return f"Compose({self.transforms})"
+    
+    def forward(self, speech, text, *args, **kwargs):
+        for t in self.transforms:
+            if isinstance(t, VoiceConversion):
+                speech = t(speech, text)
+            else:
+                x = t(speech)
+        return x
--- transformers_asr_eval.py
+++ transformers_asr_eval.py
@@ -1,10 +1,7 @@
-from datasets import load_dataset, Audio
+from datasets import load_dataset, Audio, concatenate_datasets
 from transformers import pipeline
 from transformers.pipelines.pt_utils import KeyDataset
 import torch
-import torchaudio.transforms as audio_transforms
-import torchaudio
-from torchaudio import functional as F
 import evaluate
 import numpy as np
 import pandas as pd
@@ -13,180 +10,7 @@ from tqdm import tqdm
 import os
 from copy import deepcopy
 import string
-class UniformNoise(torch.nn.Module):
-    def __init__(self, snr) -> None:
-        super().__init__()
-        self.snr = snr
-    
-    def __repr__(self):
-        return f"UniformNoise({self.snr} dB)"
-    
-    def forward(self, x, *args, **kwargs):
-        if not isinstance(x, torch.Tensor):
-            x = torch.FloatTensor(x)
-        # if not isinstance(xlen, torch.Tensor):
-        #     xlen = torch.LongTensor()
-        d = torch.empty_like(x).uniform_(-1, 1)
-        snr = torch.zeros(x.shape[:-1], device=x.device) + self.snr
-        return F.add_noise(x, d, snr)
-
-class GaussianNoise(torch.nn.Module):
-    def __init__(self, snr) -> None:
-        super().__init__()
-        self.snr = snr
-    
-    def __repr__(self):
-        return f"GaussianNoise({self.snr} dB)"
-    
-    def forward(self, x, *args, **kwargs):
-        if not isinstance(x, torch.Tensor):
-            x = torch.FloatTensor(x)
-        # if not isinstance(xlen, torch.Tensor):
-        #     xlen = torch.LongTensor()
-        d = torch.empty_like(x).normal_(0, 1)
-        snr = torch.zeros(x.shape[:-1], device=x.device) + self.snr
-        return F.add_noise(x, d, snr)
-
-class EnvNoise(torch.nn.Module):
-    seeds = [4117371, 7124264, 1832224, 8042969, 4454604, 5347561, 7059465,
-                3774329, 1412644, 1519183, 6969162, 7885564, 3707167, 5816443,
-                9477077, 9822365, 7482569, 7792808, 9120101, 5467473]
-    
-    def __init__(self, snr, noise_dir='/ocean/projects/cis220031p/mshah1/audio_robustness_benchmark/MS-SNSD/noise_test') -> None:
-        super().__init__()
-        self.snr = snr
-        self.noise_dir = noise_dir
-        self.noise_files = [x for x in os.listdir(noise_dir) if x.endswith('.wav')]
-        seed = self.seeds[int(snr % len(self.seeds))]
-        self.rng = np.random.default_rng(seed)
-    
-    def __repr__(self):
-        return f"EnvNoise({self.snr} dB)"
-
-    def forward(self, x, *args, **kwargs):
-        if not isinstance(x, torch.Tensor):
-            x = torch.FloatTensor(x)
-        xlen = x.shape[-1]
-        noise_file = os.path.join(self.noise_dir, self.noise_files[self.rng.choice(len(self.noise_files))])
-        noise_raw, sample_rate = torchaudio.load(noise_file)
-        noise = noise_raw[..., :xlen]
-        while noise.shape[-1] < xlen:
-            noise = torch.cat([noise, noise], -1)
-            noise = noise[..., :xlen]
-        noise = noise[0].reshape(-1).to(x.device)
-        snr = torch.zeros(x.shape[:-1], device=x.device) + self.snr
-        x_ = F.add_noise(x, noise, snr)
-        return x_
-
-class RIR(torch.nn.Module):
-    seed = 9983137
-    def __init__(self, sev, rir_dir='/ocean/projects/cis220031p/mshah1/audio_robustness_benchmark/RIRS_NOISES/simulated_rirs', rir_snr_file='rir_snr.csv') -> None:
-        super().__init__()
-        assert sev <= 4
-        self.rir_dir = rir_dir
-        # self.rir_files = [x for x in os.listdir(rir_dir) if x.endswith('.wav')]
-        rir_files = []
-        for root, dirs, files in os.walk(rir_dir):
-            for name in files:
-                if name.endswith('wav'):
-                    rir_files.append(os.path.join(root, name))
-        rir_snr_df = pd.read_csv(rir_snr_file)
-        unique_snrs = rir_snr_df['snr'].unique()
-        snr_sevs = np.linspace(unique_snrs.max(), unique_snrs.min(), 5)
-        print(snr_sevs, snr_sevs[sev])
-        if sev == 0:
-            filtered_rows = rir_snr_df[rir_snr_df['snr'] >= snr_sevs[sev]]
-        elif sev == 1:
-            filtered_rows = rir_snr_df[(snr_sevs[sev] <= rir_snr_df['snr']) & (rir_snr_df['snr'] <= snr_sevs[sev-1])]
-        else:
-            filtered_rows = rir_snr_df[(snr_sevs[sev] <= rir_snr_df['snr']) & (rir_snr_df['snr'] < snr_sevs[sev-1])]
-        self.rir_files = filtered_rows['filename'].values
-        print(filtered_rows["snr"].min(), filtered_rows["snr"].max())
-        print(f'using {len(self.rir_files)} rirs with average SNR={filtered_rows["snr"].mean()}')
-        # {x['filename']: x['snr'] for x in pd.read_csv(rir_snr_file).to_dict('records')}
-        self.rng = np.random.default_rng(self.seed)
-
-    def forward(self, x, *args, **kwargs):
-        if not isinstance(x, torch.Tensor):
-            x = torch.FloatTensor(x)
-        xlen = x.shape[-1]
-        def get_random_rir():
-            rir_file = os.path.join(self.rir_dir, self.rir_files[self.rng.choice(len(self.rir_files))])
-            rir_raw, sample_rate = torchaudio.load(rir_file)
-            rir = rir_raw[:, int(sample_rate * .01) : ]
-            return rir
-        rir = get_random_rir()
-        rir = rir / torch.norm(rir, p=2)
-        rir = rir[0].reshape(-1).to(x.device)
-        x_ = torchaudio.functional.fftconvolve(x, rir)
-        return x_
-
-class Speed(torch.nn.Module):
-    def __init__(self, factor, orig_freq=16000) -> None:
-        super().__init__()
-        self.factor = factor
-        self.transform = audio_transforms.Speed(orig_freq, factor)
-    
-    def forward(self, x, *args, **kwargs):
-        if not isinstance(x, torch.Tensor):
-            x = torch.FloatTensor(x)
-        xlen = x.shape[-1]
-        x_, new_lens =  self.transform.to(x.device)(x)
-        return x_
-    
-class Pitch(torch.nn.Module):
-    def __init__(self, shift, orig_freq=16000) -> None:
-        super().__init__()
-        self.shift = shift
-        self.transform = audio_transforms.PitchShift(orig_freq, shift)
-    
-    def forward(self, x, *args, **kwargs):
-        if not isinstance(x, torch.Tensor):
-            x = torch.FloatTensor(x)
-        xlen = x.shape[-1]
-        x_ =  self.transform.to(x.device)(x)
-        return x_
-
-class VoiceConversion(torch.nn.Module):
-    seed = 9983137
-    def __init__(self, accents) -> None:
-        super().__init__()
-        self.accents = accents
-        from transformers import SpeechT5Processor, SpeechT5ForTextToSpeech, SpeechT5HifiGan
-        self.processor = SpeechT5Processor.from_pretrained("microsoft/speecht5_tts")
-        self.model = SpeechT5ForTextToSpeech.from_pretrained("microsoft/speecht5_tts")
-        self.vocoder = SpeechT5HifiGan.from_pretrained("microsoft/speecht5_hifigan")
-        ds = load_dataset("Matthijs/cmu-arctic-xvectors", split="validation")
-        self.ds = ds.filter(lambda x: x['filename'].split('_')[2] in accents)
-        self.rng = np.random.default_rng(self.seed)
-    
-    def __repr__(self):
-        return f"VoiceConversion({self.accents})"
-    
-    def forward(self, speech, text, *args, **kwargs):
-        if len(self.ds) == 0:
-            return speech
-        voice_idxs = self.rng.choice(len(self.ds))
-        speaker_embeddings = torch.tensor(self.ds[voice_idxs]["xvector"]).cuda().unsqueeze(0)
-        inputs = self.processor(text=text, return_tensors="pt")
-        speech = self.model.generate_speech(inputs["input_ids"].cuda(), speaker_embeddings, vocoder=self.vocoder)
-        return speech
-    
-class Compose(torch.nn.Module):
-    def __init__(self, transforms) -> None:
-        super().__init__()
-        self.transforms = torch.nn.ModuleList(transforms)
-    
-    def __repr__(self):
-        return f"Compose({self.transforms})"
-    
-    def forward(self, speech, text, *args, **kwargs):
-        for t in self.transforms:
-            if isinstance(t, VoiceConversion):
-                speech = t(speech, text)
-            else:
-                x = t(speech)
-        return x
+from corruptions import *
 
 def normalize_transcript(txt):
     txt = txt.lower()
@@ -194,6 +18,84 @@ def normalize_transcript(txt):
     for pnc in puncs:
         txt = txt.replace(pnc, '')
     return txt
+
+def load_augmentation(args):
+    if args.augmentation:
+        aug, sev = args.augmentation.split(':', 1)
+        sev = int(sev)
+        assert sev <= 4
+    else:
+        aug = None
+        sev = 0
+    
+    if aug is None:
+        transform = None
+    elif "+" in aug:
+        augs = aug.split('+')
+        augfns = []
+        for a in augs:
+            fn, sev_args = AUGMENTATIONS[a]
+            augfns.append(fn(sev_args[min(sev, len(sev_args)-1)]))
+        transform = Compose(augfns)
+    elif aug in AUGMENTATIONS:
+        fn, sev_args = AUGMENTATIONS[aug]
+        transform = fn(sev_args[sev])
+    return transform, aug, sev
+
+def transform_dataset(dataset, transform):
+    def transform_(batch):
+        if isinstance(transform, (VoiceConversion, Compose)):
+            T = deepcopy(transform).cuda()
+        else:
+            T = transform
+        for audio, text in zip(batch['audio'], batch['text']):
+            if isinstance(transform, (VoiceConversion, Compose)):
+                audio['array'] = T(audio['array'], text)
+            else:
+                audio['array'] = T(audio['array'])
+        if isinstance(transform, VoiceConversion):
+            del T
+        return batch
+
+    nproc = 8 if isinstance(transform, VoiceConversion) else 4
+    print(dataset[0])
+    if transform is not None:
+        dataset = dataset.map(transform_, batched=True, batch_size=128, num_proc=nproc, load_from_cache_file=isinstance(transform, VoiceConversion))
+    dataset = dataset.with_format('np')
+    return dataset
+
+def transform_dataset_for_ptest(dataset, transform, num_samples, num_perturb_per_sample, subset_seed=9999):
+    def update_pert_idx(batch):
+        batch['pert_idx'] = [pert_idx] * len(batch['audio'])
+        return batch
+    
+    def transform_(batch):
+        T = transform
+        for audio in batch['audio']:
+            audio['array'] = T(audio['array'])
+        return batch
+
+    nproc =  4
+    rng = np.random.default_rng(subset_seed)
+    subset = rng.choice(len(dataset), num_samples, replace=False)
+    dataset = dataset.select(subset)
+    print(dataset[0])
+    datasets = []
+    for pert_idx in range(num_perturb_per_sample):
+        dataset = dataset.map(update_pert_idx, batched=True, batch_size=128, num_proc=nproc, load_from_cache_file=False)
+        if pert_idx == 0:            
+            datasets.append(dataset)
+        else:
+            dataset_ = dataset.map(transform_, batched=True, batch_size=128, num_proc=nproc, load_from_cache_file=False,)
+            if not isinstance(transform, (GaussianNoise, UniformNoise)):
+                dataset = dataset_
+            datasets.append(dataset_)
+        # print(datasets[0][0]['audio']['array'], datasets[-1][0]['audio']['array'])
+        # print(pert_idx, datasets[0][0]['audio']['array'] - datasets[-1][0]['audio']['array'])
+    dataset = concatenate_datasets(datasets)
+    dataset = dataset.with_format('np')
+    print(dataset[0])
+    return dataset
 
 NOISE_SNRS = [30, 10, 5, 1, -10]
 SPEEDUP_FACTORS = [1, 1.25, 1.5, 1.75, 2]
@@ -224,56 +126,18 @@ if __name__ == '__main__':
     parser.add_argument('--language', default='english')
     parser.add_argument('--output_dir', default='outputs')
     parser.add_argument('--model_parallelism', action='store_true')
-    args = parser.parse_args()     
+    parser.add_argument('--run_perturb_robustness_eval', action='store_true')
+    parser.add_argument('--n_perturb_per_sample', type=int, default=30)
+    parser.add_argument('--n_samples', type=int, default=500)
+    args = parser.parse_args()
 
-    if args.augmentation:
-        aug, sev = args.augmentation.split(':', 1)
-        sev = int(sev)
-        assert sev <= 4
-    else:
-        aug = None
-        sev = 0
-    
-    if aug is None:
-        transform = lambda x: x
-    elif "+" in aug:
-        augs = aug.split('+')
-        augfns = []
-        for a in augs:
-            fn, sev_args = AUGMENTATIONS[a]
-            augfns.append(fn(sev_args[min(sev, len(sev_args)-1)]))
-        transform = Compose(augfns)
-    elif aug in AUGMENTATIONS:
-        fn, sev_args = AUGMENTATIONS[aug]
-        transform = fn(sev_args[sev])
-
-    print(transform, aug, sev)
-    def transform_(batch):
-        # if isinstance(transform, VoiceConversion):
-        #     new_audios = transform([x['array'] for x in batch['audio']], batch['text'])
-        #     for audio, na in zip(batch['audio'], new_audios):
-        #         audio['array'] = na
-        # else:
-        if isinstance(transform, (VoiceConversion, Compose)):
-            T = deepcopy(transform).cuda()
-        else:
-            T = transform
-        for audio, text in zip(batch['audio'], batch['text']):
-            if isinstance(transform, (VoiceConversion, Compose)):
-                audio['array'] = T(audio['array'], text)
-            else:
-                audio['array'] = T(audio['array'])
-        if isinstance(transform, VoiceConversion):
-            del T
-        return batch
-
+    transform, aug, sev = load_augmentation(args)
     dataset = load_dataset(args.dataset, args.subset, split=args.split)
     dataset = dataset.cast_column("audio", Audio(sampling_rate=16_000))
-    nproc = 8 if isinstance(transform, VoiceConversion) else 4
-    print(dataset[0])
-    if aug is not None:
-        dataset = dataset.map(transform_, batched=True, batch_size=128, num_proc=nproc, load_from_cache_file=isinstance(transform, VoiceConversion))
-    dataset = dataset.with_format('np')
+    if args.run_perturb_robustness_eval:
+        dataset = transform_dataset_for_ptest(dataset, transform, args.n_samples, args.n_perturb_per_sample)
+    else:
+        dataset = transform_dataset(dataset, transform)
 
     wer_metric = evaluate.load("wer")
     cer_metric = evaluate.load("cer")
@@ -376,6 +240,8 @@ if __name__ == '__main__':
             'wer': wer,
             'cer': cer
         }
+        if 'pert_idx' in inp:
+            r['pert_idx'] = inp['pert_idx']
         output_rows.append(r)
         t.set_postfix(wer=wer, cer=cer)
 
@@ -384,4 +250,7 @@ if __name__ == '__main__':
         os.makedirs(odir)
 
     df = pd.DataFrame(output_rows)
-    df.to_csv(f'{odir}/{aug}-{sev}.tsv', sep='\t')
+    ofn = f'{aug}-{sev}'
+    if args.run_perturb_robustness_eval:
+        ofn = f'{ofn}-pertEval_{args.n_samples}_{args.n_perturb_per_sample}'
+    df.to_csv(f'{odir}/{ofn}.tsv', sep='\t')
