849a1aa123da85ccdd1d3a7e57f5c6093bc6a448
ahmed <ahmedshah1494@gmail.com>
2024-05-31 07:55:53 -0700

    updated scripts for new models and datasets

--- run_speech_robust_bench.py
+++ run_speech_robust_bench.py
@@ -2,14 +2,12 @@ from argparse import ArgumentParser
 from create_transformed_datasets import AUGMENTATIONS, PERT_ROB_AUGMENTATIONS
 from torch.cuda import device_count
 from multiprocessing import Queue, Process
-import subprocess
-import shlex
 import os
 
 def run_cmd(queue, device_id):
     while True:
         cmd = queue.get()
-        if cmd == 'None':
+        if cmd is None:
             break
         cmd = f'CUDA_VISIBLE_DEVICES={device_id} {cmd}'
         # subprocess.Call(shlex.split(cmd))
@@ -19,6 +17,7 @@ def run_cmd(queue, device_id):
 def find_path_to_univeral_adv(model, root):
     if root is None:
         print(f'WARNING: No universal adversarial perturbation directory provided for {model}. The model will not be evaluated against universal adversarial perturbations.')
+        return None
     model_name = model.split('/')[-1]
     dirpath = os.path.join(root, f'{model_name}-10')
     delta_paths = []
@@ -41,13 +40,19 @@ en_models = [
     ('openai/whisper-large-v2', 'robust_speech/advattack_data_and_results/attacks/universal/whisper-large-v2-10/1002/CKPT+2023-11-27+18-55-29+00/delta.ckpt'),
     ('facebook/wav2vec2-large-960h', 'robust_speech/advattack_data_and_results/attacks/universal/LibriSpeech/wav2vec2-large-960h-10/1002/CKPT+2024-02-01+20-14-19+00/delta.ckpt'),
     ('facebook/hubert-xlarge-ls960-ft', 'robust_speech/advattack_data_and_results/attacks/universal/LibriSpeech/hubert-xlarge-ls960-ft-10/1002/CKPT+2024-01-31+22-33-28+00/delta.ckpt'),
-    # ('openai/whisper-tiny', ''),
+    ('openai/whisper-tiny', ''),
     # ('openai/whisper-small.en', ''),
     ('openai/whisper-small', 'robust_speech/advattack_data_and_results/attacks/universal/LibriSpeech/whisper-small-10/1002/CKPT+2024-02-01+02-08-48+00/delta.ckpt'),
     # ('openai/whisper-base.en', ''),
     ('openai/whisper-base', 'robust_speech/advattack_data_and_results/attacks/universal/LibriSpeech/whisper-base-10/1002/CKPT+2024-01-31+22-19-18+00/delta.ckpt'),
     # ('openai/whisper-medium.en', ''),
     ('openai/whisper-medium', 'robust_speech/advattack_data_and_results/attacks/universal/LibriSpeech/whisper-medium-10/1002/CKPT+2024-02-01+04-07-06+00/delta.ckpt'),
+    ('facebook/mms-1b-fl102', '/jet/home/mshah1/projects/audio_robustness_benchmark/robust_speech/advattack_data_and_results/attacks/universal/MLS-ES/mms-1b-fl102-10/1002/CKPT+2024-02-01+23-21-00+00/delta.ckpt'),
+    # ('facebook/seamless-m4t-v2-large', ''),
+    # ('facebook/seamless-m4t-medium', ''),
+    ('microsoft/speecht5_asr', ''),
+    ('nvidia/canary-1b', '')
+
 ]
 
 es_models = [
@@ -56,7 +61,18 @@ es_models = [
     ('openai/whisper-tiny', '/jet/home/mshah1/projects/audio_robustness_benchmark/robust_speech/advattack_data_and_results/attacks/universal/MLS-ES/whisper-tiny-10/1002/CKPT+2024-02-01+20-45-03+00/delta.ckpt'),
     ('facebook/mms-1b-fl102', '/jet/home/mshah1/projects/audio_robustness_benchmark/robust_speech/advattack_data_and_results/attacks/universal/MLS-ES/mms-1b-fl102-10/1002/CKPT+2024-02-01+23-21-00+00/delta.ckpt'),
     ('openai/whisper-large-v2', 'robust_speech/advattack_data_and_results/attacks/universal/MLS-ES/whisper-large-v2-10/1002/CKPT+2024-02-02+09-19-36+00/delta.ckpt'),
+    ('openai/whisper-base', 'robust_speech/advattack_data_and_results/attacks/universal/MLE-ES/whisper-base-10/1002/CKPT+2024-01-31+22-19-18+00/delta.ckpt'),
+    ('nvidia/canary-1b', '')
 ]
+
+
+
+dataset2lang = {
+    'librispeech_asr': ('en', 'English'),
+    'LIUM/tedlium': ('en', 'English'),
+    'facebook/multilingual_librispeech': ('es', 'Spanish'),
+    'common_voice': ('en', 'English'),
+}
 
 parser = ArgumentParser()
 parser.add_help = True
@@ -65,20 +81,30 @@ parser.add_argument('--dataset', default="librispeech_asr")
 parser.add_argument('--subset', default=None)
 parser.add_argument('--split', default='test.clean')
 parser.add_argument('--batch_size', default=16)
+parser.add_argument('--augmentations', nargs='+')
 parser.add_argument('--run_perturb_robustness_eval', action='store_true')
 parser.add_argument('--output_dir', default='outputs', help='Output directory for the results. default: outputs')
 parser.add_argument('--skip_if_result_exists', action='store_true', help='Skip evaluation if result file for this model, augmentation and severity exists.')
 parser.add_argument('--overwrite_result_file', action='store_true')
+parser.add_argument('--run_accent_eval', action='store_true')
+parser.add_argument('--run_itw_eval', action='store_true', help='evaluate on in-the-wild data from CHiME')
 parser.add_argument('--run_universal_adv_eval', action='store_true')
 parser.add_argument('--run_universal_adv_eval_only', action='store_true')
 parser.add_argument('--universal_adv_delta_path', help='Directory containing the utterance agnoistic (universal) adversarial perturbations. The script will look for files named delta.ckpt <universal_adv_delta_path>/<model_name>. If multiple are found the full paths to the files will be lexically sorted and the last one will be selected.')
+parser.add_argument('--jobs_per_gpu', default=1)
 args = parser.parse_args()
 
 args.run_universal_adv_eval = args.run_universal_adv_eval | args.run_universal_adv_eval_only
+language = dataset2lang[args.dataset][1]
 
 def create_cmd(model, delta_path, aug, sev):
-    language = 'English' if args.dataset == 'librispeech_asr' else 'Spanish'
-    cmd = f'python evaluate_single.py --model_name {model} --batch_size {args.batch_size} --skip_if_result_exists  --dataset {args.dataset} --split {args.split} --language {language} --output_dir {args.output_dir}'
+    if aug == 'accent':
+        dataset = 'common_voice'
+    elif aug in ['itw-nf', 'itw-ff']:
+        dataset = 'chime'
+    else:
+        dataset = args.dataset
+    cmd = f'python evaluate_single.py --model_name {model} --batch_size {args.batch_size}  --dataset {dataset} --split {args.split} --language {language} --output_dir {args.output_dir}'
     if (delta_path != '') and (aug == 'universal_adv'):
         cmd += f' --universal_delta_path {delta_path}'
     if aug is not None:
@@ -89,6 +115,8 @@ def create_cmd(model, delta_path, aug, sev):
         cmd += ' --run_perturb_robustness_eval'
     if args.overwrite_result_file:
         cmd += ' --overwrite_result_file'
+    if args.skip_if_result_exists:
+        cmd += ' --skip_if_result_exists'
     return cmd    
 
 Q = Queue()
@@ -96,45 +124,66 @@ if args.models is not None:
     en_models = [m for m in en_models if m[0] in args.models]
     es_models = [m for m in es_models if m[0] in args.models]
 
-if args.dataset == 'librispeech_asr':
+if language == 'English':
     models = en_models
-else:
+elif language == 'Spanish':
     models = es_models
-
+else:
+    raise ValueError(f'Language {language} not supported')
+print(models)
 for model_data in models:
     model = model_data[0]
-    delta_path = find_path_to_univeral_adv(model, args.universal_adv_delta_path)
+    if args.run_universal_adv_eval or args.run_universal_adv_eval_only:
+        delta_path = find_path_to_univeral_adv(model, args.universal_adv_delta_path)
+    else:
+        delta_path = ''
     
+    if args.run_accent_eval:
+        cmd = create_cmd(model, delta_path, 'accent', 0)
+        Q.put(cmd)
+
+    # if args.run_itw_eval:
+    #     cmd = create_cmd(model, delta_path, 'itw-nf', 0)
+    #     Q.put(cmd)
+    #     cmd = create_cmd(model, delta_path, 'itw-ff', 0)
+    #     Q.put(cmd)
+
     if args.run_perturb_robustness_eval:
         for aug, (augcls, settings) in PERT_ROB_AUGMENTATIONS.items():
             cmd = create_cmd(model, delta_path, aug, 1)
             Q.put(cmd)
     elif args.run_universal_adv_eval_only:
-        for i in range(2, 5):
+        for i in range(1, 5):
             cmd = create_cmd(model, delta_path, 'universal_adv', i)
             Q.put(cmd)
     else:
-        if not args.run_universal_adv_eval_only:
+        if (not args.run_universal_adv_eval_only) and (args.augmentations is None):
             cmd = create_cmd(model, delta_path, None, None)
             Q.put(cmd)
         for aug, (augcls, settings) in AUGMENTATIONS.items():
-            if (aug == 'universal_adv') and (not args.run_universal_adv_eval):
+            print(aug, args.augmentations)
+            if (args.augmentations is not None) and (aug not in args.augmentations):
+                continue
+            if (aug == 'universal_adv') and ((not args.run_universal_adv_eval) or (delta_path is None)):
                 continue
             for sev, s in enumerate(settings):
                 if sev == 0:
                     continue        
                 cmd = create_cmd(model, delta_path, aug, sev)
                 Q.put(cmd)
-Q.put('None')
 
 processes =[]
-for did in range(device_count()):
-    if 'CUDA_VISIBLE_DEVICES' in os.environ:
-        did = os.environ['CUDA_VISIBLE_DEVICES'].split(',')[did]
-    p = Process(target=run_cmd, args=(Q,did))
-    p.daemon = True
-    processes.append(p)
-    p.start()
+if 'CUDA_VISIBLE_DEVICES' in os.environ:
+    devices = os.environ['CUDA_VISIBLE_DEVICES'].split(',')
+else:
+    devices = range(device_count())
+for did in devices:
+    for _ in range(int(args.jobs_per_gpu)):
+        p = Process(target=run_cmd, args=(Q,did))
+        Q.put(None)
+        p.daemon = True
+        processes.append(p)
+        p.start()
 for p in processes:
     p.join()
 
--- run_speech_robust_bench_adv.py
+++ run_speech_robust_bench_adv.py
@@ -1,10 +1,8 @@
 from argparse import ArgumentParser
-from create_transformed_datasets import AUGMENTATIONS
 from torch.cuda import device_count
 from multiprocessing import Queue, Process
-import subprocess
-import shlex
 import os
+
 
 def run_cmd(queue, device_id):
     while True:
@@ -16,60 +14,116 @@ def run_cmd(queue, device_id):
         if os.system(cmd):
             print(f'Error running {cmd}')
 
+def is_complete(data_root, dataset, model, snr, attack_type, csv_name):
+    model = model.split('/')[-1]
+    result_dir = f'{data_root}/attacks/{attack_type}/{dataset}/{model}-{snr}'
+    for root, dirs, files in os.walk(result_dir):
+        if f'cer_adv_{csv_name}.txt' in files:
+            return True
+
 en_models = [
-    # ('openai/whisper-tiny.en', 'robust_speech/advattack_data_and_results/attacks/universal/whisper-tiny.en-10/1002/CKPT+2023-11-27+19-04-38+00/delta.ckpt'),
+    ('openai/whisper-tiny.en', 'robust_speech/advattack_data_and_results/attacks/universal/whisper-tiny.en-10/1002/CKPT+2023-11-27+19-04-38+00/delta.ckpt'),
     # ('deepspeech', 'robust_speech/advattack_data_and_results/attacks/universal/deepspeech-10/1002/CKPT+2023-11-27+17-27-39+00/delta.ckpt'),
-    # ('facebook/wav2vec2-base-960h', 'robust_speech/advattack_data_and_results/attacks/universal/wav2vec2-base-960h-10/1002/CKPT+2023-11-27+15-58-14+00/delta.ckpt'),
-    # ('facebook/wav2vec2-large-960h-lv60-self', 'robust_speech/advattack_data_and_results/attacks/universal/wav2vec2-large-960h-lv60-self-10/1002/CKPT+2023-11-27+16-06-02+00/delta.ckpt'),
-    # ('facebook/hubert-large-ls960-ft', 'robust_speech/advattack_data_and_results/attacks/universal/hubert-large-ls960-ft-10/1002/CKPT+2023-11-27+15-56-27+00/delta.ckpt'),
-    # ('facebook/wav2vec2-large-robust-ft-libri-960h', 'robust_speech/advattack_data_and_results/attacks/universal/wav2vec2-large-robust-ft-libri-960h-10/1002/CKPT+2023-11-27+16-40-39+00/delta.ckpt'),
-    # ('openai/whisper-large-v2', 'robust_speech/advattack_data_and_results/attacks/universal/whisper-large-v2-10/1002/CKPT+2023-11-27+18-55-29+00/delta.ckpt'),
+    ('facebook/wav2vec2-base-960h', 'robust_speech/advattack_data_and_results/attacks/universal/wav2vec2-base-960h-10/1002/CKPT+2023-11-27+15-58-14+00/delta.ckpt'),
+    ('facebook/wav2vec2-large-960h-lv60-self', 'robust_speech/advattack_data_and_results/attacks/universal/wav2vec2-large-960h-lv60-self-10/1002/CKPT+2023-11-27+16-06-02+00/delta.ckpt'),
+    ('facebook/hubert-large-ls960-ft', 'robust_speech/advattack_data_and_results/attacks/universal/hubert-large-ls960-ft-10/1002/CKPT+2023-11-27+15-56-27+00/delta.ckpt'),
+    ('facebook/wav2vec2-large-robust-ft-libri-960h', 'robust_speech/advattack_data_and_results/attacks/universal/wav2vec2-large-robust-ft-libri-960h-10/1002/CKPT+2023-11-27+16-40-39+00/delta.ckpt'),
     ('facebook/wav2vec2-large-960h', ''),
-    # ('openai/whisper-tiny', ''),
-    # ('openai/whisper-small.en', ''),
-    # ('openai/whisper-small', ''),
-    # ('openai/whisper-base.en', ''),
-    # ('openai/whisper-base', ''),
-    # ('openai/whisper-medium.en', ''),
-    # ('openai/whisper-medium', ''),
-    # ('facebook/hubert-xlarge-ls960-ft', ''),
+    ('openai/whisper-tiny', ''),
+    ('openai/whisper-small', ''),
+    ('openai/whisper-base', ''),
+    ('openai/whisper-medium', ''),
+    ('microsoft/speecht5_asr', ''),
+    ('facebook/hubert-xlarge-ls960-ft', ''),
+    ('openai/whisper-large-v2', 'robust_speech/advattack_data_and_results/attacks/universal/whisper-large-v2-10/1002/CKPT+2023-11-27+18-55-29+00/delta.ckpt'),
+    ('nvidia/canary-1b', ''),
+    ('facebook/mms-1b-fl102', ''),
 ]
 
 es_models = [
     ('facebook/wav2vec2-large-xlsr-53-spanish', ''),
     ('facebook/wav2vec2-base-10k-voxpopuli-ft-es',''),
+    ('openai/whisper-tiny', ''),
+    ('openai/whisper-base', ''),
+    ('nvidia/canary-1b', ''),
     ('facebook/mms-1b-fl102', ''),
     ('openai/whisper-large-v2', ''),
-    ('openai/whisper-tiny', ''),
 ]
+
+dataset2lang = {
+    'LibriSpeech': ('en', 'English'),
+    'TEDLIUM': ('en', 'English'),
+    'MLS-ES': ('es', 'Spanish'),
+}
+
 PGD_SNRS = [40, 30, 20, 10]
-UNIVERSAL_SNRS = [10]
+UNIVERSAL_SNRS = [40,30,20,10]
 parser = ArgumentParser()
 parser.add_argument('--models', nargs='+', default=None, help='List of models to run. Models must be present in en_models or es_models in run_speech_robust_bench_adv.py')
 parser.add_argument('--dataset', default="LibriSpeech", help='Dataset to run the attack on. This should be the name of a directory in the <data_root>/data.')
 parser.add_argument('--data_root', default="robust_speech/advattack_data_and_results")
 parser.add_argument('--data_csv_name', default="test-clean", help='Name of the csv file in the <data_root>/data/<dataset>/csv directory. DO NOT INCLUDE THE FILE EXTENSION.')
-parser.add_argument('--attack_type', default="pgd", help='Type of attack to run. Options: pgd, universal', choices=['pgd', 'universal'])
+parser.add_argument('--attack_type', default="pgd", help='Type of attack to run. Options: pgd, universal', choices=['pgd', 'universal', 'cw'])
+parser.add_argument('--jobs_per_gpu', default=1)
 args = parser.parse_args()
 
+language = dataset2lang[args.dataset][1]
+
 def create_pgd_cmd(model, snr):
-    repo, model = model.split('/')
-    cmd = f'python evaluate.py attack_configs/LibriSpeech/pgd/hf.yaml --root={args.data_root} --model_repo={repo} --model_name={model} --snr={snr} --dataset {args.dataset} --data_csv_name {args.data_csv_name}'
-    if args.dataset == 'MLS-ES' and model in ['mms-1b-fl102', 'whisper-large-v2', 'whisper-tiny']:
-        if model == 'mms-1b-fl102':
-            cmd += ' --lang spa'
-        else:
+    if model == 'deepspeech':
+        cmd = f'python evaluate.py attack_configs/LibriSpeech/pgd/deepspeech.yaml --root={args.data_root} --snr={snr} --dataset {args.dataset} --data_csv_name {args.data_csv_name}'
+        if args.dataset == 'TEDLIUM':
+            cmd += ' --tokenizer_file tokenizer_uncased'
+    elif model == 'nvidia/canary-1b':
+        cmd = f'python evaluate.py attack_configs/LibriSpeech/pgd/canary-1b.yaml --root={args.data_root} --snr={snr} --dataset {args.dataset} --data_csv_name {args.data_csv_name}'
+        if args.dataset == 'MLS-ES':
             cmd += ' --lang es'
+    else:
+        repo, model = model.split('/')
+        cmd = f'python evaluate.py attack_configs/LibriSpeech/pgd/hf.yaml --root={args.data_root} --model_repo={repo} --model_name={model} --snr={snr} --dataset {args.dataset} --data_csv_name {args.data_csv_name}'
+        if args.dataset == 'MLS-ES' and model in ['mms-1b-fl102', 'whisper-large-v2', 'whisper-tiny', 'whisper-base']:
+            if model == 'mms-1b-fl102':
+                cmd += ' --lang spa'
+            else:
+                cmd += ' --lang es'
     return cmd
 
 def create_universal_cmd(model, snr):
-    repo, model = model.split('/')
-    cmd = f'python fit_attacker.py attack_configs/LibriSpeech/universal/hf.yaml --root={args.data_root} --model_repo={repo} --model_name={model} --snr={snr} --dataset {args.dataset} --data_csv_name {args.data_csv_name}'
-    if args.dataset == 'MLS-ES':
-        if model == 'mms-1b-fl102':
-            cmd += ' --lang spa'
-        else:
+    if model == 'deepspeech':
+        cmd = f'python fit_attacker.py attack_configs/LibriSpeech/universal/deepspeech.yaml --root={args.data_root} --snr={snr} --dataset {args.dataset} --data_csv_name {args.data_csv_name}'
+        if args.dataset == 'TEDLIUM':
+            cmd += ' --tokenizer_file tokenizer_uncased'
+    elif model == 'nvidia/canary-1b':
+        cmd = f'python fit_attacker.py attack_configs/LibriSpeech/universal/canary-1b.yaml --root={args.data_root} --snr={snr} --dataset {args.dataset} --data_csv_name {args.data_csv_name}'
+        if args.dataset == 'MLS-ES':
             cmd += ' --lang es'
+    else:
+        repo, model = model.split('/')
+        cmd = f'python fit_attacker.py attack_configs/LibriSpeech/universal/hf.yaml --root={args.data_root} --model_repo={repo} --model_name={model} --snr={snr} --dataset {args.dataset} --data_csv_name {args.data_csv_name}'
+        if args.dataset == 'MLS-ES'  and model in ['mms-1b-fl102', 'whisper-large-v2', 'whisper-tiny', 'whisper-base']:
+            if model == 'mms-1b-fl102':
+                cmd += ' --lang spa'
+            else:
+                cmd += ' --lang es'
+    return cmd
+
+def create_cw_cmd(model):
+    if model == 'deepspeech':
+        cmd = f'python evaluate.py attack_configs/LibriSpeech/cw/hf.yaml --root={args.data_root} --dataset {args.dataset} --data_csv_name {args.data_csv_name}'
+        if args.dataset == 'TEDLIUM':
+            cmd += ' --tokenizer_file tokenizer_uncased'
+    if model == 'nvidia/canary-1b':
+        cmd = f'python evaluate.py attack_configs/LibriSpeech/cw/canary-1b.yaml --root={args.data_root} --dataset {args.dataset} --data_csv_name {args.data_csv_name}'
+        if args.dataset == 'MLS-ES':
+            cmd += ' --lang es'
+    else:
+        repo, model = model.split('/')
+        cmd = f'python evaluate.py attack_configs/LibriSpeech/cw/hf.yaml --root={args.data_root} --model_repo={repo} --model_name={model} --dataset {args.dataset} --data_csv_name {args.data_csv_name}'
+        if args.dataset == 'MLS-ES' and model in ['mms-1b-fl102', 'whisper-large-v2', 'whisper-tiny', 'whisper-base']:
+            if model == 'mms-1b-fl102':
+                cmd += ' --lang spa'
+            else:
+                cmd += ' --lang es'
     return cmd
 
 def create_attack_cmd(model, snr):
@@ -77,6 +131,8 @@ def create_attack_cmd(model, snr):
         return create_pgd_cmd(model, snr)
     elif args.attack_type == 'universal':
         return create_universal_cmd(model, snr)
+    elif args.attack_type == 'cw':
+        return create_cw_cmd(model)
     else:
         raise ValueError(f'Invalid attack type {args.attack_type}')
 
@@ -85,27 +141,41 @@ if args.models is not None:
     en_models = [m for m in en_models if m[0] in args.models]
     es_models = [m for m in es_models if m[0] in args.models]
 
-if args.dataset == 'LibriSpeech':
+if language == 'English':
     models = en_models
-else:
+elif language == 'Spanish':
     models = es_models
+else:
+    raise ValueError(f'Language {language} not supported')
 
-SNRS = UNIVERSAL_SNRS if args.attack_type == 'universal' else PGD_SNRS
+SNRS = {
+    'pgd': PGD_SNRS,
+    'universal': UNIVERSAL_SNRS,
+    'cw': [None]
+}[args.attack_type]
 for model_data in models:
     model, delta_path = model_data
     for snr in SNRS:
-        cmd = create_attack_cmd(model, snr)
-        Q.put(cmd)
+        if is_complete(args.data_root, args.dataset, model, snr, args.attack_type, args.data_csv_name):
+            print(f'{model} at SNR {snr} is already complete. Skipping...')
+            continue
+        else:
+            print(f'Adding {model} at SNR {snr} to queue...')
+            cmd = create_attack_cmd(model, snr)
+            Q.put(cmd)
 Q.put('None')
 
 processes =[]
-for did in range(device_count()):
-    if 'CUDA_VISIBLE_DEVICES' in os.environ:
-        did = os.environ['CUDA_VISIBLE_DEVICES'].split(',')[did]
-    p = Process(target=run_cmd, args=(Q,did))
-    p.daemon = True
-    processes.append(p)
-    p.start()
+if 'CUDA_VISIBLE_DEVICES' in os.environ:
+    devices = os.environ['CUDA_VISIBLE_DEVICES'].split(',')
+else:
+    devices = range(device_count())
+for did in devices:
+    for _ in range(int(args.jobs_per_gpu)):
+        p = Process(target=run_cmd, args=(Q,did))
+        p.daemon = True
+        processes.append(p)
+        p.start()
 for p in processes:
     p.join()
 
