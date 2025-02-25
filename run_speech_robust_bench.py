de57caef98f7d4b76291fab77ed6c9aa9e27fb83
ahmed <ahmedshah1494@gmail.com>
2025-02-25 11:04:59 -0800

    more models and languages

--- run_speech_robust_bench.py
+++ run_speech_robust_bench.py
@@ -33,34 +33,51 @@ def find_path_to_univeral_adv(model, root, snr=10):
         return None
 
 en_models = [
-    ('openai/whisper-tiny.en', 'robust_speech/advattack_data_and_results/attacks/universal/whisper-tiny.en-10/1002/CKPT+2023-11-27+19-04-38+00/delta.ckpt'),
-    ('deepspeech', 'robust_speech/advattack_data_and_results/attacks/universal/deepspeech-10/1002/CKPT+2023-11-27+17-27-39+00/delta.ckpt'),
-    ('facebook/wav2vec2-base-960h', 'robust_speech/advattack_data_and_results/attacks/universal/wav2vec2-base-960h-10/1002/CKPT+2023-11-27+15-58-14+00/delta.ckpt'),
-    ('facebook/wav2vec2-large-960h-lv60-self', 'robust_speech/advattack_data_and_results/attacks/universal/wav2vec2-large-960h-lv60-self-10/1002/CKPT+2023-11-27+16-06-02+00/delta.ckpt'),
-    ('facebook/hubert-large-ls960-ft', 'robust_speech/advattack_data_and_results/attacks/universal/hubert-large-ls960-ft-10/1002/CKPT+2023-11-27+15-56-27+00/delta.ckpt'),
-    ('facebook/wav2vec2-large-robust-ft-libri-960h', 'robust_speech/advattack_data_and_results/attacks/universal/wav2vec2-large-robust-ft-libri-960h-10/1002/CKPT+2023-11-27+16-40-39+00/delta.ckpt'),
-    ('openai/whisper-large-v2', 'robust_speech/advattack_data_and_results/attacks/universal/whisper-large-v2-10/1002/CKPT+2023-11-27+18-55-29+00/delta.ckpt'),
-    ('facebook/wav2vec2-large-960h', 'robust_speech/advattack_data_and_results/attacks/universal/LibriSpeech/wav2vec2-large-960h-10/1002/CKPT+2024-02-01+20-14-19+00/delta.ckpt'),
-    ('facebook/hubert-xlarge-ls960-ft', 'robust_speech/advattack_data_and_results/attacks/universal/LibriSpeech/hubert-xlarge-ls960-ft-10/1002/CKPT+2024-01-31+22-33-28+00/delta.ckpt'),
+    ('openai/whisper-tiny.en', ''),
+    ('deepspeech', ''),
+    ('facebook/wav2vec2-base-960h', ''),
+    ('facebook/wav2vec2-large-960h-lv60-self', ''),
+    ('facebook/hubert-large-ls960-ft', ''),
+    ('facebook/wav2vec2-large-robust-ft-libri-960h', ''),
+    ('openai/whisper-large-v2', ''),
+    ('facebook/wav2vec2-large-960h', ''),
+    ('facebook/hubert-xlarge-ls960-ft', ''),
     ('openai/whisper-tiny', ''),
     # ('openai/whisper-small.en', ''),
-    ('openai/whisper-small', 'robust_speech/advattack_data_and_results/attacks/universal/LibriSpeech/whisper-small-10/1002/CKPT+2024-02-01+02-08-48+00/delta.ckpt'),
+    ('openai/whisper-small', ''),
     # ('openai/whisper-base.en', ''),
-    ('openai/whisper-base', 'robust_speech/advattack_data_and_results/attacks/universal/LibriSpeech/whisper-base-10/1002/CKPT+2024-01-31+22-19-18+00/delta.ckpt'),
+    ('openai/whisper-base', ''),
     # ('openai/whisper-medium.en', ''),
-    ('openai/whisper-medium', 'robust_speech/advattack_data_and_results/attacks/universal/LibriSpeech/whisper-medium-10/1002/CKPT+2024-02-01+04-07-06+00/delta.ckpt'),
-    ('facebook/mms-1b-fl102', '/jet/home/mshah1/projects/audio_robustness_benchmark/robust_speech/advattack_data_and_results/attacks/universal/MLS-ES/mms-1b-fl102-10/1002/CKPT+2024-02-01+23-21-00+00/delta.ckpt'),
-    # ('facebook/seamless-m4t-v2-large', ''),
-    # ('facebook/seamless-m4t-medium', ''),
+    ('openai/whisper-medium', ''),
+    ('facebook/mms-1b-fl102', ''),
     ('microsoft/speecht5_asr', ''),
-    ('nvidia/canary-1b', '')
-
+    ('nvidia/canary-1b', ''),
+    ('nvidia/parakeet-ctc-1.1b', ''),
+    ('nvidia/parakeet-rnnt-0.6b', ''),
+    ('nvidia/parakeet-rnnt-1.1b', ''),
+    ('nvidia/parakeet-ctc-0.6b', ''),
 ]
 
 es_models = [
-    ('facebook/wav2vec2-large-xlsr-53-spanish', '/jet/home/mshah1/projects/audio_robustness_benchmark/robust_speech/advattack_data_and_results/attacks/universal/MLS-ES/wav2vec2-large-xlsr-53-spanish-10/1002/CKPT+2024-01-31+22-26-37+00/delta.ckpt'),
-    ('facebook/wav2vec2-base-10k-voxpopuli-ft-es','/jet/home/mshah1/projects/audio_robustness_benchmark/robust_speech/advattack_data_and_results/attacks/universal/MLS-ES/wav2vec2-base-10k-voxpopuli-ft-es-10/1002/CKPT+2024-01-31+23-42-07+00/delta.ckpt'),
-    ('openai/whisper-tiny', '/jet/home/mshah1/projects/audio_robustness_benchmark/robust_speech/advattack_data_and_results/attacks/universal/MLS-ES/whisper-tiny-10/1002/CKPT+2024-02-01+20-45-03+00/delta.ckpt'),
+    ('facebook/wav2vec2-large-xlsr-53-spanish', ''),
+    ('facebook/wav2vec2-base-10k-voxpopuli-ft-es',''),
+    ('openai/whisper-tiny', ''),
+    ('facebook/mms-1b-fl102', ''),
+    ('openai/whisper-large-v2', ''),
+    ('openai/whisper-base', ''),
+    ('nvidia/canary-1b', '')
+]
+
+du_models = [
+    # ('openai/whisper-tiny', '/jet/home/mshah1/projects/audio_robustness_benchmark/robust_speech/advattack_data_and_results/attacks/universal/MLS-ES/whisper-tiny-10/1002/CKPT+2024-02-01+20-45-03+00/delta.ckpt'),
+    ('facebook/mms-1b-fl102', '/jet/home/mshah1/projects/audio_robustness_benchmark/robust_speech/advattack_data_and_results/attacks/universal/MLS-ES/mms-1b-fl102-10/1002/CKPT+2024-02-01+23-21-00+00/delta.ckpt'),
+    ('openai/whisper-large-v2', 'robust_speech/advattack_data_and_results/attacks/universal/MLS-ES/whisper-large-v2-10/1002/CKPT+2024-02-02+09-19-36+00/delta.ckpt'),
+    ('openai/whisper-base', 'robust_speech/advattack_data_and_results/attacks/universal/MLE-ES/whisper-base-10/1002/CKPT+2024-01-31+22-19-18+00/delta.ckpt'),
+    ('nvidia/canary-1b', '')
+]
+
+fr_models = [
+    # ('openai/whisper-tiny', '/jet/home/mshah1/projects/audio_robustness_benchmark/robust_speech/advattack_data_and_results/attacks/universal/MLS-ES/whisper-tiny-10/1002/CKPT+2024-02-01+20-45-03+00/delta.ckpt'),
     ('facebook/mms-1b-fl102', '/jet/home/mshah1/projects/audio_robustness_benchmark/robust_speech/advattack_data_and_results/attacks/universal/MLS-ES/mms-1b-fl102-10/1002/CKPT+2024-02-01+23-21-00+00/delta.ckpt'),
     ('openai/whisper-large-v2', 'robust_speech/advattack_data_and_results/attacks/universal/MLS-ES/whisper-large-v2-10/1002/CKPT+2024-02-02+09-19-36+00/delta.ckpt'),
     ('openai/whisper-base', 'robust_speech/advattack_data_and_results/attacks/universal/MLE-ES/whisper-base-10/1002/CKPT+2024-01-31+22-19-18+00/delta.ckpt'),
@@ -68,12 +85,21 @@ es_models = [
 ]
 
 
-
 dataset2lang = {
-    'librispeech_asr': ('en', 'English'),
-    'LIUM/tedlium': ('en', 'English'),
-    'facebook/multilingual_librispeech': ('es', 'Spanish'),
-    'common_voice': ('en', 'English'),
+    'librispeech_asr': {
+            None: ('en', 'English')
+        },
+    'LIUM/tedlium': {
+            'release3': ('en', 'English')
+        },
+    'facebook/multilingual_librispeech': {
+            'spanish': ('es', 'Spanish'),
+            'german': ('de', 'German'),
+            'french': ('fr', 'French')
+        },
+    'common_voice': {
+            None: ('en', 'English')
+        },
 }
 
 parser = ArgumentParser()
@@ -84,6 +110,7 @@ parser.add_argument('--subset', default=None)
 parser.add_argument('--split', default='test.clean')
 parser.add_argument('--batch_size', default=16)
 parser.add_argument('--augmentations', nargs='+')
+parser.add_argument('--language', default=None)
 parser.add_argument('--run_perturb_robustness_eval', action='store_true')
 parser.add_argument('--output_dir', default='outputs', help='Output directory for the results. default: outputs')
 parser.add_argument('--skip_if_result_exists', action='store_true', help='Skip evaluation if result file for this model, augmentation and severity exists.')
@@ -93,11 +120,15 @@ parser.add_argument('--run_itw_eval', action='store_true', help='evaluate on in-
 parser.add_argument('--run_universal_adv_eval', action='store_true')
 parser.add_argument('--run_universal_adv_eval_only', action='store_true')
 parser.add_argument('--universal_adv_delta_path', help='Directory containing the utterance agnoistic (universal) adversarial perturbations. The script will look for files named delta.ckpt <universal_adv_delta_path>/<model_name>. If multiple are found the full paths to the files will be lexically sorted and the last one will be selected.')
+parser.add_argument('--force_retransform', action='store_true', help='If True, do not retrieve perturbed data from hf_repo. default: False')
 parser.add_argument('--jobs_per_gpu', default=1)
 args = parser.parse_args()
 
 args.run_universal_adv_eval = args.run_universal_adv_eval | args.run_universal_adv_eval_only
-language = dataset2lang[args.dataset][1]
+if args.language is not None:
+    language = args.language
+else:
+    language = dataset2lang[args.dataset][args.subset][1]
 
 def create_cmd(model, delta_path, aug, sev):
     if aug == 'accent':
@@ -121,19 +152,28 @@ def create_cmd(model, delta_path, aug, sev):
         cmd += ' --overwrite_result_file'
     if args.skip_if_result_exists:
         cmd += ' --skip_if_result_exists'
+    if args.force_retransform:
+        cmd += ' --force_retransform'
+    if dataset == 'facebook/multilingual_librispeech' and (aug is None):
+        cmd += ' --text_field transcript'
     return cmd    
 
 Q = Queue()
-if args.models is not None:
-    en_models = [m for m in en_models if m[0] in args.models]
-    es_models = [m for m in es_models if m[0] in args.models]
 
 if language == 'English':
     models = en_models
 elif language == 'Spanish':
     models = es_models
+elif language == 'German':
+    models = du_models
+elif language == 'French':
+    models = fr_models
 else:
     raise ValueError(f'Language {language} not supported')
+
+if args.models is not None:
+    models = [m for m in models if m[0] in args.models]
+
 print(models)
 for model_data in models:
     model = model_data[0]
@@ -142,11 +182,18 @@ for model_data in models:
     if args.run_accent_eval:
         cmd = create_cmd(model, delta_path, 'accent', 0)
         Q.put(cmd)
+    # continue
+
+    if args.run_itw_eval:
+        for aug in ['itw_nf', 'itw_ff', 'itw_nf_ami', 'itw_ff_ami']:
+            cmd = create_cmd(model, delta_path, aug, 0)
+            Q.put(cmd)
 
     if args.run_perturb_robustness_eval:
         for aug, settings in PERT_ROB_AUGMENTATIONS.items():
-            cmd = create_cmd(model, delta_path, aug, 1)
-            Q.put(cmd)
+            for i in range(1, 5):
+                cmd = create_cmd(model, delta_path, aug, i)
+                Q.put(cmd)
     elif args.run_universal_adv_eval_only:
         for i in range(1, 5):
             snrs = AUGMENTATIONS['universal_adv']
@@ -155,7 +202,7 @@ for model_data in models:
             cmd = create_cmd(model, delta_path, 'universal_adv', i)
             Q.put(cmd)
     else:
-        if (not args.run_universal_adv_eval_only) and (args.augmentations is None):
+        if (not args.run_universal_adv_eval_only) and (args.augmentations is None) or args.run_perturb_robustness_eval:
             cmd = create_cmd(model, delta_path, None, None)
             Q.put(cmd)
         for aug, settings in AUGMENTATIONS.items():
@@ -168,6 +215,7 @@ for model_data in models:
                 if sev == 0:
                     continue        
                 cmd = create_cmd(model, delta_path, aug, sev)
+                print(cmd)
                 Q.put(cmd)
 
 processes =[]
