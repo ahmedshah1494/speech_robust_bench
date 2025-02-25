e72ec097621c419f9dcb2feddc53731bd26e26b6
ahmed <ahmedshah1494@gmail.com>
2025-02-25 11:05:56 -0800

    import as needed

--- models/__init__.py
+++ models/__init__.py
@@ -1,20 +1,28 @@
-from models import canary
-from models import deepspeech
-from models import hf
-from models import mms
-from models import rnnt
-from models import whisper
-
 def create_model_pipeline(model, dataset, batch_size=1, language='English', **kwargs):
     if model == 'deepspeech':
+        from models import deepspeech
         return deepspeech.create_model_pipeline(dataset, batch_size=batch_size, **kwargs)
     elif model == 'rnnt':
+        from models import rnnt
         return rnnt.create_model_pipeline(dataset, batch_size=batch_size, **kwargs)
+    elif model.startswith('sb_') or model.startswith('speechbrain/'):
+        from models import speechbrain
+        return speechbrain.create_model_pipeline(model, dataset, batch_size=batch_size, **kwargs)
     elif model.startswith('nvidia/canary'):
+        from models import canary
         return canary.create_model_pipeline(dataset, batch_size=batch_size, language=language, **kwargs)
+    elif model.startswith('nvidia/parakeet'):
+        from models import parakeet
+        return parakeet.create_model_pipeline(model, dataset, batch_size=batch_size, language=language, **kwargs)
     elif model.startswith('openai/whisper'):
+        from models import whisper
         return whisper.create_model_pipeline(dataset, model, batch_size=batch_size, language=language, **kwargs)
     elif model.startswith('facebook/mms'):
+        from models import mms
         return mms.create_model_pipeline(dataset, model, batch_size=batch_size, language=language, **kwargs)
+    elif model.startswith('Qwen/Qwen2-Audio-7B'):
+        from models import qwen_audio
+        return qwen_audio.create_model_pipeline(dataset, model, batch_size=batch_size, language=language, **kwargs)
     else:
+        from models import hf
         return hf.create_model_pipeline(dataset, model, batch_size=batch_size, **kwargs)
