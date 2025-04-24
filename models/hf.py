f663a0fcfa64f26dba3dec444a5e1d0731409ecf
ahmed <ahmedshah1494@gmail.com>
2025-04-23 22:17:08 -0700

    specifying chunk_lenght_s for whisper

--- models/hf.py
+++ models/hf.py
@@ -3,6 +3,8 @@ from transformers.pipelines.pt_utils import KeyDataset
 import torch
 
 def create_model_pipeline(dataset, model, batch_size=1, gen_kwargs={}, **kwargs):
+    if model.startswith('openai/whisper'):
+        kwargs['chunk_length_s'] = 30
     pipe = pipeline("automatic-speech-recognition", model=model, batch_size=batch_size, torch_dtype=torch.float16, **kwargs, generate_kwargs=gen_kwargs)
     pipe = pipe(KeyDataset(dataset, "audio"))
     return pipe
